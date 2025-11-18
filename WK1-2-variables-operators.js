fd2ea28094ba16a6e018ed1c2b251d607ca36597
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2025-11-18 10:34:32 +0100

    added class files on variables and operators

---
+++ WK1-2-variables-operators.js
@@ -0,0 +1,108 @@
+var fname = "Niyi";
+console.log(fname);
+
+fname = "Olaniyi";
+console.log(fname);
+
+let firstName = "Jelil";
+
+// you cannot redeclare a variable when using let
+// let firstName = "Waliu";
+
+const password = 1234;
+
+// you cannot reassign value to a const variable
+// password = 12345678;
+
+var age = 11;
+var age = 20;
+console.log(age);
+
+let middleName = "Korede";
+console.log(middleName);
+
+{
+    let middleName = "Tayo";
+    console.log(middleName);
+}
+
+console.log(middleName);
+
+let lastName = "Sumonu";
+console.log(lastName);
+
+{
+    console.log(lastName);
+}
+
+{
+    let fruit = "apple";
+    console.log(fruit);
+    
+}
+
+// you cannot access fruit here because it is not a global variable
+// console.log(fruit);
+
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+
+let a = 10;
+console.log(a++);
+console.log(a);
+
+let b = 12;
+console.log(++b);
+console.log(b);
+
+
+let c = 10;
+console.log(c--);
+console.log(c);
+
+let d = 12;
+console.log(--d);
+console.log(d);
+
+let e = 10;
+let f = 2;
+
+e += f;
+console.log(e);
+
+let g = 12;
+let i = 3;
+g -= i;
+console.log(g);
+
+let j = 12;
+j *= 2;
+console.log(j);
+
+
+let k = 12;
+k /= 2;
+console.log(k);
+
+
+let l = 12;
+l **= 2;
+console.log(l);
+
+let m = 12;
+m %= 2;
+console.log(m);
+
+
+
+
+
+
