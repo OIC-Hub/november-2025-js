de06a6ea9da3399ce21cb1a2e709b5fc90b5809a
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2025-11-19 10:48:07 +0100

    added class files on operators and datatypes

---
+++ WK1-3-operators-datatypes.js
@@ -0,0 +1,65 @@
+console.log(5 > 3);
+console.log(5 > 5);
+console.log(-1 > -3);
+console.log(5 > 9);
+console.log(5 < 3);
+console.log(2 < 3);
+console.log(5 >= 3);
+console.log(5 >= 5);
+console.log(5 >= 7);
+console.log(5 <= 3);
+console.log(5 == 3);
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 === 5);
+console.log(5 === "5");
+
+console.log(5 != 3);
+console.log(5 != 5);
+console.log(5 != "5");
+console.log(5 !== 5);
+console.log(5 !== "5");
+
+console.log(5 > 3 && 7 > 9);
+console.log(5 > 3 && 11 > 9);
+
+console.log(5 > 3 || 7 > 9);
+console.log(1 > 3 || 7 > 9);
+
+console.log(!(5 > 3));
+
+console.log(5 > 3 ? 154 : 156);
+console.log(5 < 3 ? 5154 : 56);
+
+
+let a = "Hi, my friend.";
+let b = 12;
+let c = 12.5;
+
+let sentence = "Hi, can I have your brother's shoe?";
+let sentence2 = 'This is "really" important';
+let sentence3 = `This is David's word that it is "really" important`;
+
+let firstName = "Ola";
+let lastName = "Niyi";
+
+// let fullName = firstName + " " + lastName;
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let age = 12;
+let ageSentence = `She is ${age} years old`;
+console.log(ageSentence);
+
+console.log(2 + 4 + 5 + "is");
+console.log("is" + 1 + 3 + 4);
+console.log("2" + 2);
+console.log(2 + 4 + "is" + 1 + 7);
+
+let aa = 5;
+console.log(typeof aa);
+aa = "John";
+console.log(typeof aa);
+aa = true;
+console.log(typeof aa);
+
